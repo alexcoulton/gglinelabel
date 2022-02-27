e3d896c30c488e47a937d42ad97247af0285042b
Alex Coulton <alexcoulton@gmail.com>
2022-02-27 15:01:17 +0000

    initial commit for gglinelabel

---
+++ R/geom_linelabel.R
@@ -0,0 +1,581 @@
+load_all('~/work/external.repos/ggplot2_testing/')
+library(reshape2)
+source('~/work/ucl/scripts/misc/functions.R')
+library(devtools)
+library(ggplot2testing)
+
+
+#devtools::install('~/work/external.repos/ggplot2_testing/')
+#source('~/work/external.repos/ggplot2_testing/R/geom-point.r')
+#source('~/work/external.repos/ggplot2_testing/R/layer.r')
+
+
+chr1 = get.genes(1, 0, 1000000000000)
+chr1$expression = rnorm(nrow(chr1), mean = 100, sd = 20)
+chr1$expression[chr1$expression < 0] = 0
+chr1 = chr1[1:100, ]
+
+head(chr1)
+
+label.df = chr1[c('gene', 'expression')]
+head(label.df)
+
+test_function = function(){
+    layer_data(data)
+}
+
+head(chr1)
+
+
+
+
+
+linelabel_chr = chr1[c('start', 'expression', 'gene')]
+colnames(linelabel_chr) = c('x', 'y', 'label')
+
+make.line.df.fan = function(data.df){
+    data.df2 = data.df[c('x', 'y')]
+    data.df2$linelabelx = seq(
+        min(data.df2$x),
+        max(data.df2$x),
+        max(data.df2$x) / nrow(data.df2)
+    )
+
+    data.df2$linelabely = max(data.df2$y) + (max(data.df2$y) / 5)
+
+    data.df2$group = 1:nrow(data.df2)
+    order(data.df2$x)
+
+    verticallinedf = data.df2
+    colnames(verticallinedf) = c('x1', 'y1', 'x2', 'y2', 'group')
+    verticallinedf1 = verticallinedf[c('x1', 'y1', 'group')]
+    verticallinedf2 = verticallinedf[c('x2', 'y2', 'group')]
+    colnames(verticallinedf1) = c('x', 'y', 'group')
+    colnames(verticallinedf2) = c('x', 'y', 'group')
+    verticallinedf2$x = verticallinedf1$x
+    verticallinedf2$y = max(data.df2$y) + (max(data.df2$y) / 10)
+
+    vert.line.df = bind_rows(verticallinedf1, verticallinedf2)
+
+    linelabeldf = data.df2
+    colnames(linelabeldf) = c('x1', 'y1', 'x2', 'y2', 'group')
+    linelabeldf1 = linelabeldf[c('x1', 'y1', 'group')]
+    linelabeldf2 = linelabeldf[c('x2', 'y2', 'group')]
+    colnames(linelabeldf1) = c('x', 'y', 'group')
+    colnames(linelabeldf2) = c('x', 'y', 'group')
+
+    linelabeldf1$y = max(data.df2$y) + (max(data.df2$y) / 10)
+
+    data.df3 = bind_rows(linelabeldf1, linelabeldf2)
+    data.df3$label = data.df$label
+    data.df3
+
+    data.df4 = bind_rows(vert.line.df, data.df3)
+    #data.df4[data.df4$group == 6, ]
+    data.df4
+}
+
+
+make.line.df.star = function(data.df){
+    data.df2 = data.df[c('x', 'y')]
+    data.df2$linelabelx = seq(
+        min(data.df2$x),
+        max(data.df2$x),
+        max(data.df2$x) / nrow(data.df2)
+    )
+
+    data.df2$linelabely = max(data.df2$y) + (max(data.df2$y) / 5)
+
+    data.df2$group = 1:nrow(data.df2)
+    order(data.df2$x)
+
+    linelabeldf = data.df2
+    colnames(linelabeldf) = c('x1', 'y1', 'x2', 'y2', 'group')
+    linelabeldf1 = linelabeldf[c('x1', 'y1', 'group')]
+    linelabeldf2 = linelabeldf[c('x2', 'y2', 'group')]
+    colnames(linelabeldf1) = c('x', 'y', 'group')
+    colnames(linelabeldf2) = c('x', 'y', 'group')
+
+
+    data.df3 = bind_rows(linelabeldf1, linelabeldf2)
+    data.df3$label = data.df$label
+    data.df3
+}
+
+make.line.df.cloud = function(data.df){
+    data.df2 = data.df[c('x', 'y')]
+    data.df2 = data.df[1:2, ]
+    data.df2$x = c(min(data.df$x), max(data.df$x))
+    data.df2$y = c(max(data.df$y) + max(data.df$y) / 10, max(data.df$y) + max(data.df$y) / 10)
+    data.df2
+}
+
+
+make.line.df.cloud2 = function(data.df){
+    data.df
+    head(data.df)
+    #data.df2 = data.df[c('x', 'y')]
+    x2 = seq(
+        min(data.df$x),
+        max(data.df$x),
+        max(data.df$x) / nrow(data.df)
+    )
+
+    y2 = max(data.df$y) + (max(data.df$y) / 5)
+
+    data.df$group = 1:nrow(data.df)
+    order(data.df$x)
+
+    linelabeldf1 = data.df
+    linelabeldf1$y = max(data.df$y) + max(data.df$y) / 10
+
+    linelabeldf2 = data.df
+    linelabeldf2$x = x2
+    linelabeldf2$y = y2
+
+    data.df3 = bind_rows(linelabeldf1, linelabeldf2)
+    data.df3
+}
+
+make.line.df.cloud3 = function(data.df){
+    data.df2 = data.df[c('x', 'y')]
+
+    data.df2$y = max(data.df$y) + max(data.df$y) / 10
+    data.df2
+}
+
+make.label.df = function(data.df){
+    data.df$y = max(data.df$y) + max(data.df$y) / 5
+    data.df$x = seq(
+        min(data.df$x),
+        max(data.df$x),
+        max(data.df$x) / nrow(data.df)
+    )
+    data.df
+}
+
+StatCloud1 = ggproto('StatCloud1', Stat,
+    compute_group = function(data, scales){
+        make.line.df.cloud(data)
+    },
+    required_aes = c('x', 'y', 'group')
+)
+
+StatCloud2 = ggproto('StatCloud2', Stat,
+    compute_group = function(data, scales){
+        make.line.df.cloud2(data)
+    },
+    required_aes = c('x', 'y', 'group')
+)
+
+StatCloud3 = ggproto('StatCloud3', Stat,
+    compute_group = function(data, scales){
+        make.line.df.cloud3(data)
+    },
+    required_aes = c('x', 'y', 'group')
+)
+
+
+stat_cloud1 <- function(mapping = NULL, data = NULL, geom = "path",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatCloud1, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+
+stat_cloud2 <- function(mapping = NULL, data = NULL, geom = "path",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatCloud2, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+stat_cloud3 <- function(mapping = NULL, data = NULL, geom = "point",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatCloud3, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+StatFan = ggproto('StatFan', Stat,
+    compute_group = function(data, scales){
+        make.line.df.fan(data)
+    },
+    required_aes = c('x', 'y', 'group')
+)
+
+stat_fan <- function(mapping = NULL, data = NULL, geom = "path",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatFan, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+
+StatStar = ggproto('StatStar', Stat,
+    compute_group = function(data, scales){
+        make.line.df.star(data)
+    },
+    required_aes = c('x', 'y', 'group')
+)
+
+stat_star <- function(mapping = NULL, data = NULL, geom = "path",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatStar, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+
+StatLineLabel = ggproto('StatLineLabel', Stat,
+    compute_group = function(data, scales){
+        labeldf = make.label.df(data)
+    },
+    required_aes = c('x', 'y')
+)
+
+stat_linelabel <- function(mapping = NULL, data = NULL, geom = "text",
+                       position = "identity", na.rm = FALSE, show.legend = NA, 
+                       inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatLineLabel, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+############################
+#GROB 
+############################
+
+GeomSimplePoint <- ggproto("GeomSimplePoint", Geom,
+    required_aes = c("x", "y"),
+    default_aes = aes(shape = 19, colour = "black", linetype = 'solid',
+        text.size = 3.88, angle = 0, hjust = 0.5,
+        vjust = 0.5, alpha = NA, family = "", fontface = 1, lineheight = 1.2
+        ),
+    draw_key = draw_key_point,
+
+    draw_panel = function(data, panel_params, coord , arrow = NULL,
+        lineend = "butt", linejoin = "round", linemitre = 10,
+        na.rm = FALSE, linetype = 'solid'){ 
+       ############################
+       #GEOM_PATH DRAW FUNCTION 
+       ############################ 
+        if (!anyDuplicated(data$group)) {
+          message_wrap("geom_path: Each group consists of only one observation. ",
+            "Do you need to adjust the group aesthetic?")
+        }
+
+        # must be sorted on group
+        data.cloud2 = make.line.df.cloud2(data)
+        data.cloud2 <- data.cloud2[order(data.cloud2$group), , drop = FALSE]
+        munched <- coord_munch(coord, data.cloud2, panel_params)
+
+        # Silently drop lines with less than two points, preserving order
+        rows <- stats::ave(seq_len(nrow(munched)), munched$group, FUN = length)
+        munched <- munched[rows >= 2, ]
+        if (nrow(munched) < 2) return(zeroGrob())
+
+        # Work out grouping variables for grobs
+        n <- nrow(munched)
+        group_diff <- munched$group[-1] != munched$group[-n]
+        start <- c(TRUE, group_diff)
+        end <-   c(group_diff, TRUE)
+
+        id <- match(munched$group, unique(munched$group))
+        path_elements = polylineGrob(
+            munched$x, munched$y, id = id,
+            default.units = "native", arrow = arrow,
+            gp = gpar(
+              col = alpha(munched$colour, munched$alpha)[start],
+              fill = alpha(munched$colour, munched$alpha)[start],
+              #lwd = munched$size[start] * .pt,
+              lwd = 1,
+              lty = munched$linetype[start],
+              lineend = lineend,
+              linejoin = linejoin,
+              linemitre = linemitre
+            )
+        )
+
+        cloud1 = make.line.df.cloud(data)
+        cloud2 = make.line.df.cloud2(data)
+        cloud3 = make.line.df.cloud3(data)
+
+        data.cloud = make.line.df.cloud(data)
+        #data.cloud = data
+        coords <- coord$transform(data.cloud, panel_params)
+        #coords <- coord$transform(data, panel_params)
+
+        lwd1 = coords$size * .pt
+        if(length(lwd1) == 0) lwd1 = NULL
+        line_elements = grid::linesGrob(
+            coords$x, coords$y, 
+            default.units = "native",
+            gp = grid::gpar(
+                col = coords$colour,
+                lwd = lwd1,
+                lty = coords$linetype
+            )
+        )
+
+        data.cloud3 = make.line.df.cloud3(data)
+        coords <- coord$transform(data.cloud3, panel_params)
+        point_elements = grid::pointsGrob(
+            coords$x, coords$y,
+            #pch = coords$shape,
+            size = unit(1, 'mm'),
+            gp = grid::gpar(col = coords$colour)
+        )
+
+        ############################
+        #LABEL ELEMENTS 
+        ############################
+
+        data.label = make.label.df(data)
+
+        lab <- data.label$label
+        lab <- parse_safe(as.character(lab))
+
+        data.label <- coord$transform(data.label, panel_params)
+
+        if (is.character(data.label$vjust)) {
+          data.label$vjust <- compute_just(data.label$vjust, data.label$y, data.label$x, data.label$angle)
+        }
+        if (is.character(data.label$hjust)) {
+          data.label$hjust <- compute_just(data.label$hjust, data.label$x, data.label$y, data.label$angle)
+        }
+
+
+
+        label_elements = textGrob(
+          lab,
+          data.label$x, data.label$y, default.units = "native",
+          hjust = data.label$hjust, vjust = data.label$vjust,
+          rot = data.label$angle,
+          gp = gpar(
+            col = alpha(data.label$colour, data.label$alpha),
+            fontsize = data.label$text.size * .pt,
+            fontfamily = data.label$family,
+            fontface = data.label$fontface,
+            lineheight = data.label$lineheight
+          )
+        )
+
+
+
+
+
+        gList(line_elements, point_elements, path_elements, label_elements)
+        #path_elements
+    }
+)
+
+geom_simple_point <- function(mapping = NULL, data = NULL, stat = "identity",
+                              position = "identity", na.rm = FALSE, show.legend = NA, 
+                              inherit.aes = TRUE, ...) {
+  layer(
+    geom = GeomSimplePoint, mapping = mapping,  data = data, stat = stat, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+plot1 = ggplot(chr1, aes(x = start, y = expression, label = gene)) + 
+    geom_point() +
+    geom_line() +
+    geom_simple_point(angle = 90, hjust = 0, text.size = 2) +
+    #geom_simple_point(colour = '#d8d654', linetype = 'dashed') +
+    coord_cartesian(ylim = c(0, 200))
+
+pl(plot1)
+
+
+    #geom_text(aes(label = gene))
+    #stat_star(linetype = 'dotted', color = '#888888') +
+    stat_fan(linetype = 'dotted', color = '#888888') +
+    #stat_cloud1() +
+    #stat_cloud2(linetype = 'dotted', color = '#888888') +
+    #stat_cloud3() +
+    stat_linelabel(angle = 90, hjust = 0, size = 2) 
+    #coord_cartesian(ylim = c(0, 250))
+    #stat_alex(linetype = 'dotted', color = '#888888')
+    #stat_chull(fill = NA, color = 'black')
+
+
+pl(plot1)
+
+
+
+plot1 = ggplot(chr1, aes(x = start, y = expression, label = gene)) + 
+    geom_point() +
+    geom_line() +
+    #geom_text(aes(label = gene))
+    #stat_star(linetype = 'dotted', color = '#888888') +
+    stat_fan(linetype = 'dotted', color = '#888888') +
+    #stat_cloud1() +
+    #stat_cloud2(linetype = 'dotted', color = '#888888') +
+    #stat_cloud3() +
+    stat_linelabel(angle = 90, hjust = 0, size = 2) 
+    #coord_cartesian(ylim = c(0, 250))
+    #stat_alex(linetype = 'dotted', color = '#888888')
+    #stat_chull(fill = NA, color = 'black')
+
+
+pl(plot1)
+
+
+
+geom_simple_point <- function(mapping = NULL, data = NULL, stat = "identity",
+                              position = "identity", na.rm = FALSE, show.legend = NA, 
+                              inherit.aes = TRUE, ...) {
+  layer(
+    geom = GeomSimplePoint, mapping = mapping,  data = data, stat = stat, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+GeomLongFunction <- ggproto("GeomLongFunction", Geom,
+    required_aes = c("x", "y"),
+    default_aes = aes(
+	colour = NA, fill = "grey20", size = 0.5,
+	linetype = 1, alpha = 1
+    ),
+    draw_key = draw_key_abline,
+
+    draw_group = function(data, panel_params, coord,
+			  component = c("m1", "m2", "m3", "m4", "m5")) {
+	component <- match.arg(component, several.ok = TRUE)
+	result_grob <- lapply(component,function(comp){
+	    data_comp <- data
+	    data_comp$y <- data[,comp]
+	    coords <- coord$transform(data_comp, panel_params)
+	    grid::linesGrob(
+		coords$x, coords$y, 
+		default.units = "native",
+		gp = grid::gpar(
+		    col = coords$colour,
+		    lwd = coords$size * .pt,
+		    lty = coords$linetype
+		)
+	    )
+	})
+	do.call(grid::gList, result_grob)
+    }
+)
+
+geom_longfunction <- function(mapping = NULL, data = NULL, stat = "long_function",
+                              position = "identity", show.legend = NA, 
+                              inherit.aes = TRUE,
+                              component = c("m1", "m2", "m3", "m4", "m5"), ...) {
+    layer(
+        geom = GeomLongFunction, mapping = mapping, data = data, stat = stat, 
+        position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+        params = list(component = component, ...)
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+plot1 = ggplot(chr1, aes(x = start, y = expression, label = gene)) + 
+    geom_point() +
+    geom_line() +
+    #geom_text(aes(label = gene))
+    stat_star(linetype = 'dotted', color = '#888888') +
+    #stat_fan(linetype = 'dotted', color = '#888888') +
+    #stat_cloud1() +
+    #stat_cloud2(linetype = 'dotted', color = '#888888') +
+    #stat_cloud3() +
+    stat_linelabel(angle = 90, hjust = 0, size = 2) 
+    #coord_cartesian(ylim = c(0, 250))
+    #stat_alex(linetype = 'dotted', color = '#888888')
+    #stat_chull(fill = NA, color = 'black')
+
+
+pl(plot1)
+
+
+plot1 = ggplot(chr1, aes(x = start, y = expression, label = gene)) + 
+    geom_point() +
+    geom_line() +
+    stat_star(linetype = 'dotted', color = '#888888')
+    #stat_chull(fill = NA, color = 'black')
+
+plot1 = ggplot(chr1, aes(x = start, y = expression, label = gene)) + 
+    geom_point() +
+    geom_line() +
+    stat_fan(linetype = 'dotted', color = '#888888')
+
+pl(plot1)
+
+layer_data(plot1)
+
+layer_data
+pl(plot1)
+
+
+
+
+geom_linelabel <- function(label.df){
+    head(chr1)
+}
+
+geom_linelabel(chr1)
+
+
+
+############################
+#HW REF CODE 
+############################
+
+#https://cran.r-project.org/web/packages/ggplot2/vignettes/extending-ggplot2.html
+
+#StatChull <- ggproto("StatChull", Stat,
+  #compute_group = function(data, scales) {
+    #data[chull(data$x, data$y), , drop = FALSE]
+  #},
+  
+  #required_aes = c("x", "y")
+#)
+
+#stat_chull <- function(mapping = NULL, data = NULL, geom = "line",
+                       #position = "identity", na.rm = FALSE, show.legend = NA, 
+                       #inherit.aes = TRUE, ...) {
+  #layer(
+    #stat = StatChull, data = data, mapping = mapping, geom = geom, 
+    #position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    #params = list(na.rm = na.rm, ...)
+  #)
+#}
+
